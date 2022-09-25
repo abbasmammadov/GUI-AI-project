e23f73a447737b27cb3d8f853fdc380f0f0652d9
Kaleb Mesfin Asfaw <kalebmes06@gmail.com>
2022-09-25 21:46:46 +0900

    add changes

--- ML_model/frames.py
+++ ML_model/frames.py
@@ -1,7 +1,10 @@
 import cv2
 import os
-video_path = '/Users/kalebmesfinasfaw/Desktop/VS Code Collections/GUI-AI-project/ML_model/data'
+ROOT_DIR = os.path.abspath(os.curdir) # directory = '/Users/kalebmesfin06/Desktop/VS Code Collections'
+# video_path = '/Users/kalebmesfinasfaw/Desktop/VS Code Collections/GUI-AI-project/ML_model/data'
+video_path = os.path.join(ROOT_DIR, 'GUI-AI-project', 'ML_model', 'data')
 vid_name = 'ny5s_test_pyqt.mp4'
+print(video_path)
 def getFrame(video_path, vidcap, sec, count):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
@@ -18,13 +21,14 @@ def vid_to_frame(video_path, vid_name, sec=0, frameRate=0.5):
     #//it will capture image in each 'frameRate' second
     count=1
     success, image_path = getFrame(video_path, vidcap, sec, count)
-    image_folder = [image_path.split('/')[-1]]
+    image_folder = [image_path]
     while success:
         count = count + 1
         sec = sec + frameRate
         sec = round(sec, 2)
         success, image_path = getFrame(video_path, vidcap, sec, count)
-        image_folder.append(image_path.split('/')[-1])
+        image_folder.append(image_path)
     return image_folder
-print(vid_to_frame(video_path, vid_name))
+# print(vid_to_frame(video_path, vid_name))
+
 
