67c7791bb5e451634947faddf4f76c52deb92181
Kaleb Mesfin Asfaw <kalebmes06@gmail.com>
2022-10-06 09:11:33 +0900

    modify server file

--- server.py
+++ server.py
@@ -5,22 +5,19 @@ host = '0.0.0.0'
 port = 999
 ser.bind((host, port))
 ser.listen(3)
-while True:
+end = False
+while not end:
     c, address = ser.accept()
     print(' : ' + c.recv(1024).decode())
-    brk = True
     while True:
          mess = input('Enter message : ')
          if mess == 'exit':
-             brk = False
-         if not brk:
+             end = True
+         if end:
             break
 
          c.send(bytes(mess, 'utf-8'))
          print(' : ' + c.recv(1024).decode())
-    if not brk:
-        break
-
 
 
 c.close()
